5e4cd5485f01faddf25f01e1e52382d3ee31c9c6
SnyderMbishai <csmbishai@gmail.com>
2020-07-18 14:00:00 +0300

    Configure routing

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { Route } from 'react-router';
+import { Switch, BrowserRouter } from 'react-router-dom';
+import { TodoApp } from './components/index';
+import Hello from './components/hello';
+import Reverse from './components/reverser';
+import ToDo from './components/todo';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render(){
+    return(
+      <React.Fragment>
+        <BrowserRouter>
+          <Switch>
+            <Route path="/" component={TodoApp} exact/>
+            <Route path="/hello" component={Hello} exact/>
+            <Route path="/reverse" component={Reverse} exact/>
+            <Route path="/todo" component={ToDo} />
+          </Switch>
+        </BrowserRouter>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
