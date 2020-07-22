b3faa6907281a0070f664047bb5f0662935f8590
SnyderMbishai <csmbishai@gmail.com>
2020-07-22 12:35:29 +0300

    Update URLs

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router';
 import { Switch, BrowserRouter } from 'react-router-dom';
-import { TodoApp } from './components/index';
+import Welcome from './components/index';
 import Hello from './components/hello';
 import Reverse from './components/reverser';
 import ToDo from './components/todo';
@@ -12,10 +12,10 @@ class App extends Component {
       <React.Fragment>
         <BrowserRouter>
           <Switch>
-            <Route path="/" component={TodoApp} exact/>
-            <Route path="/hello" component={Hello} exact/>
+            <Route path="/" component={Welcome} exact/>
+            <Route path="/hello" component={Hello} exact />
             <Route path="/reverse" component={Reverse} exact/>
-            <Route path="/todo" component={ToDo} />
+            <Route path="/todo" component={ToDo} exact/>
           </Switch>
         </BrowserRouter>
       </React.Fragment>
